a458937281131b7e4931bf894cea0eaa267055bc
Nathan Roe <nate1234zach@gmail.com>
2021-02-28 15:42:18 -0700

    fixed routing and code allocation

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
+const express = require('express');
 const app = express();
-app.use(cors());
-/* Ensure any requests prefixed with /static will serve our "client/static" directory */
-app.use("/client", express.static(path.resolve(__dirname, "client", "views")));
+const path = require('path');
+app.use(express.static("client/public"));
 
-
-/* Redirect all routes to our (soon to exist) "index.html" file */
-app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("client/views", "index.html"));
-});
-
-app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
+app.route('/*')
+    .get(function (req, res) {
+        res.sendFile(path.join(__dirname + '/client/views/index.html'));
+    })
+const port = 3000;
+app.listen(port, () => console.log(`Listening at port: ${port}`));
