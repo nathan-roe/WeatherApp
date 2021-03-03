04bd6f6f3eebe23907dfa6201d91607e6e73d4b8
Nathan Roe <nate1234zach@gmail.com>
2021-03-02 17:26:19 -0700

    added weather particals and fixed chart labels

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -156,7 +156,6 @@ $(document).ready(() => {
         retina_detect: true
       });
 });
-
 // initialize map and make api requests on click of map/autocomplete
 function initMap(styles=[], graphColor={"border":'rgb(255,198,145)', "background":'rgb(120,169,204)'}) {
     $(".data") != undefined ? $(".data").remove() : ''; 
@@ -373,6 +372,246 @@ function initMap(styles=[], graphColor={"border":'rgb(255,198,145)', "background
 }
 // given the weather str from api return object with svg and full name
 const alterWeather = str => {
+    const rain = () => {
+        $("#weather") !== undefined ? $(".weatherWrapper").remove() : '';
+        $(".data").append("<div class='weatherWrapper'><div id='weather'></div></div>");
+        tsParticles.load("weather", {
+            detectRetina: true,
+            fpsLimit: 60,
+            interactivity: {
+            detectsOn: "canvas",
+            events: {
+                onClick: {
+                enable: false,
+                mode: "repulse"
+                },
+                onDiv: {
+                elementId: "repulse-div",
+                enable: false,
+                mode: "repulse"
+                },
+                onHover: {
+                enable: false,
+                mode: "repulse",
+                parallax: {
+                    enable: false,
+                    force: 60,
+                    smooth: 10
+                }
+                },
+                resize: true
+            },
+            modes: {
+                bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+                speed: 3
+                },
+                connect: {
+                distance: 80,
+                lineLinked: {
+                    opacity: 0.5
+                },
+                radius: 60
+                },
+                grab: {
+                distance: 400,
+                lineLinked: {
+                    opacity: 1
+                }
+                },
+                push: {
+                quantity: 4
+                },
+                remove: {
+                quantity: 2
+                },
+                repulse: {
+                distance: 200,
+                duration: 0.4
+                }
+            }
+            },
+            particles: {
+            color: {
+                value: "#ffffff"
+            },
+            lineLinked: {
+                blink: false,
+                color: "#000",
+                consent: false,
+                distance: 150,
+                enable: false,
+                opacity: 0,
+                width: 0
+            },
+            rotate: {
+                value: 0,
+                random: false,
+                direction: "clockwise",
+                animation: {
+                enable: false,
+                speed: 5,
+                sync: false
+                }
+            },
+            move: {
+                attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+                },
+                bounce: false,
+                direction: "bottom",
+                enable: true,
+                outMode: "out",
+                random: true,
+                speed: 30,
+                straight: true
+            },
+            number: {
+                density: {
+                enable: true,
+                area: 800
+                },
+                limit: 0,
+                value: 200
+            },
+            opacity: {
+                animation: {
+                enable: false,
+                minimumValue: 0.1,
+                speed: 1,
+                sync: false
+                },
+                random: false,
+                value: 0.5
+            },
+            shape: {
+                character: {
+                fill: false,
+                font: "Verdana",
+                style: "",
+                value: "*",
+                weight: "400"
+                },
+                image: [],
+                polygon: {
+                nb_sides: 5
+                },
+                stroke: {
+                color: "#efefefee",
+                width: 1
+                },
+                type: "line"
+            },
+            size: {
+                animation: {
+                enable: false,
+                minimumValue: 0.1,
+                speed: 40,
+                sync: false
+                },
+                random: false,
+                value: 5
+            }
+            },
+            polygon: {
+            draw: {
+                enable: false,
+                lineColor: "#ffffff",
+                lineWidth: 0.5
+            },
+            move: {
+                radius: 10
+            },
+            scale: 1,
+            type: "none",
+            url: ""
+            }
+        });
+    }
+    const snow = () => {
+        $("#weather") !== undefined ? $(".weatherWrapper").remove() : '';
+        $(".data").append("<div class='weatherWrapper'><div id='weather'></div></div>");
+        $(".weatherWrapper").append("<div id='weather'></div>");
+        
+        tsParticles.load("weather", {
+            fps_limit: 60,
+            interactivity: {
+            detect_on: "canvas",
+            events: {
+                
+            },
+            modes: {
+                bubble: { distance: 400, duration: 0.3, opacity: 1, size: 1, speed: 3 },
+                grab: { distance: 400, line_linked: { opacity: 0.5 } },
+                push: { particles_nb: 4 },
+                remove: { particles_nb: 2 },
+                repulse: { distance: 200, duration: 0.4 }
+            }
+            },
+            particles: {
+            color: { value: "#fff" },
+            line_linked: {
+                color: "#ffffff",
+                distance: 500,
+                enable: false,
+                opacity: 0.4,
+                width: 1
+            },
+            move: {
+                attract: { enable: false, rotateX: 600, rotateY: 1200 },
+                bounce: false,
+                direction: "bottom",
+                enable: true,
+                out_mode: "out",
+                random: false,
+                size: true,
+                speed: 3,
+                straight: false
+            },
+            number: { density: { enable: true, value_area: 800 }, value: 200 },
+            opacity: {
+                anim: { enable: false, opacity_min: 0.1, speed: 1, sync: false },
+                random: true,
+                value: 0.5
+            },
+            shape: {
+                character: {
+                fill: false,
+                font: "Verdana",
+                style: "",
+                value: "*",
+                weight: "400"
+                },
+                image: {
+                height: 100,
+                replace_color: true,
+                src: "images/github.svg",
+                width: 100
+                },
+                polygon: { nb_sides: 5 },
+                stroke: { color: "#000000", width: 0 },
+                type: "circle"
+            },
+            size: {
+                anim: { enable: false, size_min: 0.1, speed: 40, sync: false },
+                random: true,
+                value: 10
+            }
+            },
+            polygon: {
+            draw: { enable: false, lineColor: "#ffffff", lineWidth: 0.5 },
+            move: { radius: 10 },
+            scale: 1,
+            type: "none",
+            },
+            retina_detect: true
+        });
+    }
     switch(str){
         case 'clear':
             return {'name': "Clear Skies", 'svg': `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-brightness-high-fill" viewBox="0 0 16 16">
@@ -392,25 +631,30 @@ const alterWeather = str => {
             </svg>`};
             break;
         case 'lightrain':
+            rain();
             return {'name': "Light Rain", 'svg': `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-cloud-drizzle-fill" viewBox="0 0 16 16">
                 <path d="M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm-3.5 1.5a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm.747-8.498a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973z"/>
             </svg>`};
             break;
         case 'rain':
+            rain();
             return {'name': 'Rain', 'svg': `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-cloud-rain-heavy-fill" viewBox="0 0 16 16">
                 <path d="M4.176 11.032a.5.5 0 0 1 .292.643l-1.5 4a.5.5 0 0 1-.936-.35l1.5-4a.5.5 0 0 1 .644-.293zm3 0a.5.5 0 0 1 .292.643l-1.5 4a.5.5 0 0 1-.936-.35l1.5-4a.5.5 0 0 1 .644-.293zm3 0a.5.5 0 0 1 .292.643l-1.5 4a.5.5 0 0 1-.936-.35l1.5-4a.5.5 0 0 1 .644-.293zm3 0a.5.5 0 0 1 .292.643l-1.5 4a.5.5 0 0 1-.936-.35l1.5-4a.5.5 0 0 1 .644-.293zm.229-7.005a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 10H13a3 3 0 0 0 .405-5.973z"/>
             </svg>`};
             break;
         case 'oshower':
+            rain();
             return {'name': 'Rain', 'svg': `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-umbrella-fill" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 0a.5.5 0 0 1 .5.5v.514C12.625 1.238 16 4.22 16 8c0 0 0 .5-.5.5-.149 0-.352-.145-.352-.145l-.004-.004-.025-.023a3.484 3.484 0 0 0-.555-.394A3.166 3.166 0 0 0 13 7.5c-.638 0-1.178.213-1.564.434a3.484 3.484 0 0 0-.555.394l-.025.023-.003.003s-.204.146-.353.146-.352-.145-.352-.145l-.004-.004-.025-.023a3.484 3.484 0 0 0-.555-.394 3.3 3.3 0 0 0-1.064-.39V13.5H8h.5v.039l-.005.083a2.958 2.958 0 0 1-.298 1.102 2.257 2.257 0 0 1-.763.88C7.06 15.851 6.587 16 6 16s-1.061-.148-1.434-.396a2.255 2.255 0 0 1-.763-.88 2.958 2.958 0 0 1-.302-1.185v-.025l-.001-.009v-.003s0-.002.5-.002h-.5V13a.5.5 0 0 1 1 0v.506l.003.044a1.958 1.958 0 0 0 .195.726c.095.191.23.367.423.495.19.127.466.229.879.229s.689-.102.879-.229c.193-.128.328-.304.424-.495a1.958 1.958 0 0 0 .197-.77V7.544a3.3 3.3 0 0 0-1.064.39 3.482 3.482 0 0 0-.58.417l-.004.004S5.65 8.5 5.5 8.5c-.149 0-.352-.145-.352-.145l-.004-.004a3.482 3.482 0 0 0-.58-.417A3.166 3.166 0 0 0 3 7.5c-.638 0-1.177.213-1.564.434a3.482 3.482 0 0 0-.58.417l-.004.004S.65 8.5.5 8.5C0 8.5 0 8 0 8c0-3.78 3.375-6.762 7.5-6.986V.5A.5.5 0 0 1 8 0z"/>
             </svg>`};
 
         case 'ishower':
+            snow();
             return {'name': 'Snow and Rain', 'svg': `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-cloud-snow-fill" viewBox="0 0 16 16">
                 <path d="M2.625 11.5a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm2.75 2a.25.25 0 0 1 .25.25v.57l.5-.287a.25.25 0 0 1 .249.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm5.5 0a.25.25 0 0 1 .25.25v.57l.5-.287a.25.25 0 0 1 .249.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 0 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm-2.75-2a.25.25 0 0 1 .25.25v.57l.5-.287a.25.25 0 0 1 .249.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm5.5 0a.25.25 0 0 1 .25.25v.57l.5-.287a.25.25 0 0 1 .249.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 0 1-.5 0v-.57l-.501.287a.25.25 0 1 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm-.22-7.223a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 10.25H13a3 3 0 0 0 .405-5.973z"/>
             </svg>`}
         case 'snow':
+            snow();
             return {'name': 'Snow', 'svg': `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-cloud-snow-fill" viewBox="0 0 16 16">
                 <path d="M2.625 11.5a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm2.75 2a.25.25 0 0 1 .25.25v.57l.5-.287a.25.25 0 0 1 .249.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm5.5 0a.25.25 0 0 1 .25.25v.57l.5-.287a.25.25 0 0 1 .249.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 0 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm-2.75-2a.25.25 0 0 1 .25.25v.57l.5-.287a.25.25 0 0 1 .249.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm5.5 0a.25.25 0 0 1 .25.25v.57l.5-.287a.25.25 0 0 1 .249.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 0 1-.5 0v-.57l-.501.287a.25.25 0 1 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm-.22-7.223a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 10.25H13a3 3 0 0 0 .405-5.973z"/>
             </svg>`}
@@ -419,6 +663,7 @@ const alterWeather = str => {
                 <path d="M13.5 0a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V7.5h-1.5a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V15h-1.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V.5a.5.5 0 0 0-.5-.5h-2zM7 1.5l.364-.343a.5.5 0 0 0-.728 0l-.002.002-.006.007-.022.023-.08.088a28.458 28.458 0 0 0-1.274 1.517c-.769.983-1.714 2.325-2.385 3.727C2.368 7.564 2 8.682 2 9.733 2 12.614 4.212 15 7 15s5-2.386 5-5.267c0-1.05-.368-2.169-.867-3.212-.671-1.402-1.616-2.744-2.385-3.727a28.458 28.458 0 0 0-1.354-1.605l-.022-.023-.006-.007-.002-.001L7 1.5zm0 0l-.364-.343L7 1.5zm-.016.766L7 2.247l.016.019c.24.274.572.667.944 1.144.611.781 1.32 1.776 1.901 2.827H4.14c.58-1.051 1.29-2.046 1.9-2.827.373-.477.706-.87.945-1.144zM3 9.733c0-.755.244-1.612.638-2.496h6.724c.395.884.638 1.741.638 2.496C11 12.117 9.182 14 7 14s-4-1.883-4-4.267z"/>
             </svg>`}
         case 'lightsnow':
+            snow();
             return {'name': 'Light Snow', 'svg': `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-cloud-snow" viewBox="0 0 16 16">
                 <path d="M13.405 4.277a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 10.25H13a3 3 0 0 0 .405-5.973zM8.5 1.25a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1-.001 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4.002 4.002 0 0 1 8.5 1.25zM2.625 11.5a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm2.75 2a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm5.5 0a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm-2.75-2a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm5.5 0a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25z"/>
             </svg>`}
@@ -427,6 +672,7 @@ const alterWeather = str => {
                 <path d="M7.053 11.276A.5.5 0 0 1 7.5 11h1a.5.5 0 0 1 .474.658l-.28.842H9.5a.5.5 0 0 1 .39.812l-2 2.5a.5.5 0 0 1-.875-.433L7.36 14H6.5a.5.5 0 0 1-.447-.724l1-2zm6.352-7.249a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 10H13a3 3 0 0 0 .405-5.973z"/>
             </svg>`}
         case 'tsrain':
+            rain();
             return {'name': 'Thunder and Rain', 'svg': `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-cloud-lightning-rain-fill" viewBox="0 0 16 16">
                 <path d="M2.658 11.026a.5.5 0 0 1 .316.632l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.316zm9.5 0a.5.5 0 0 1 .316.632l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.316zm-7.5 1.5a.5.5 0 0 1 .316.632l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.316zm9.5 0a.5.5 0 0 1 .316.632l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.316zm-7.105-1.25A.5.5 0 0 1 7.5 11h1a.5.5 0 0 1 .474.658l-.28.842H9.5a.5.5 0 0 1 .39.812l-2 2.5a.5.5 0 0 1-.875-.433L7.36 14H6.5a.5.5 0 0 1-.447-.724l1-2zm6.352-7.249a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 10H13a3 3 0 0 0 .405-5.973z"/>
             </svg>`}
